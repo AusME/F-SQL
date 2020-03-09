1efa0388dc1d184a63709818b93bd0a26a843a4e
ausme <ausme2@gmail.com>
2020-03-08 20:08:30 -0600

    Finished StringCaseUtil and test.

--- src/main/java/name/ben/shepley/fsql/util/StringCaseUtil.java
+++ src/main/java/name/ben/shepley/fsql/util/StringCaseUtil.java
@@ -2,17 +2,16 @@ package name.ben.shepley.fsql.util;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public enum StringCaseUtil {
     CAMEL {
         @Override
-        public String convert(StringCaseUtil from, String string) {
+        public String transform(StringCaseUtil from, String string) {
             String[] words = from.parse(string);
             for (int i = 0; i < words.length; i ++) {
-                if (i == 0) {
-                    words[i] = words[i].toLowerCase();
-                } else {
-                    words[i] = words[i].toLowerCase();
+                words[i] = words[i].toLowerCase();
+                if (i != 0) {
                     char[] letters = words[i].toCharArray();
                     letters[0] = Character.toUpperCase(letters[0]);
                     words[i] = new String(letters);
@@ -20,19 +19,14 @@ public enum StringCaseUtil {
             }
 
             StringBuilder stringBuilder = new StringBuilder();
-            for (String s : words) {
-                stringBuilder.append(s);
-            }
-
+            Stream.of(words).forEach(stringBuilder::append);
             return stringBuilder.toString();
         }
 
         @Override
         public String[] parse(String string) {
-            if (string == null || string.isEmpty()) {
-                return new String[0];
-            } else if (!this.is(string)) {
-                return new String[0];
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be camelCase.");
             }
 
             char[] letters = string.toCharArray();
@@ -54,103 +48,208 @@ public enum StringCaseUtil {
 
         @Override
         public boolean is(String string) {
-            if (string == null || string.isEmpty()) {
-                return false;
+            return string != null && !string.isEmpty() && string.matches("^[a-z]+([A-Z][a-z]+)+$");
+        }
+
+        @Override
+        public String append(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be camelCase.");
             }
 
+            char[] letters = snippet.toLowerCase().toCharArray();
+            letters[0] = Character.toUpperCase(letters[0]);
+            return string + new String(letters);
+        }
+
+        @Override
+        public String prepend(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be camelCase.");
+            }
+
+            snippet = snippet.toLowerCase();
             char[] letters = string.toCharArray();
-
-            if (!Character.isLowerCase(letters[0])) {
-                return false;
-            }
-
-            for (int i = 1; i < string.length(); i++) {
-                if (!Character.isLowerCase(letters[i]) && !Character.isUpperCase(letters[i])) {
-                    return false;
-                }
-            }
-
-            return true;
+            letters[0] = Character.toUpperCase(letters[0]);
+            return snippet + new String(letters);
         }
 
     },
     PASCAL {
         @Override
-        public String convert(StringCaseUtil from, String string) {
+        public String transform(StringCaseUtil from, String string) {
+            String[] words = from.parse(string);
 
-            return null;
+            for (int i = 0; i < words.length; i ++) {
+                words[i] = words[i].toLowerCase();
+                char[] letters = words[i].toCharArray();
+                letters[0] = Character.toUpperCase(letters[0]);
+                words[i] = new String(letters);
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            Stream.of(words).forEach(stringBuilder::append);
+            return stringBuilder.toString();
         }
 
         @Override
         public String[] parse(String string) {
-            if (string == null || string.isEmpty()) {
-                return new String[0];
-            } else if (!this.is(string)) {
-                return new String[0];
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be PascalCase.");
             }
 
             char[] letters = string.toCharArray();
             List<String> words = new LinkedList<>();
             StringBuilder wordBuilder = new StringBuilder();
-            for (int i = 0; i < string.length(); i++) {
 
+            wordBuilder.append(letters[0]);
+            for (int i = 1; i < string.length(); i++) {
+                if (Character.isLowerCase(letters[i])) {
+                    wordBuilder.append(letters[i]);
+                } else if (Character.isUpperCase(letters[i])) {
+                    words.add(wordBuilder.toString());
+                    wordBuilder = new StringBuilder();
+                    wordBuilder.append(letters[i]);
+                }
             }
+            words.add(wordBuilder.toString());
 
             return words.toArray(new String[0]);
         }
 
         @Override
         public boolean is(String string) {
-            return false;
+            return string != null && !string.isEmpty() && string.matches("^([A-Z][a-z]+)+$");
+        }
+
+        @Override
+        public String append(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be PascalCase.");
+            }
+
+            char[] letters = snippet.toLowerCase().toCharArray();
+            letters[0] = Character.toUpperCase(letters[0]);
+            return string + new String(letters);
+        }
+
+        @Override
+        public String prepend(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be PascalCase.");
+            }
+
+            snippet = snippet.toLowerCase();
+            char[] letters = snippet.toCharArray();
+            letters[0] = Character.toUpperCase(letters[0]);
+            return new String(letters) + string;
         }
 
     },
     SNAKE {
         @Override
-        public String convert(StringCaseUtil from, String string) {
-            return null;
+        public String transform(StringCaseUtil from, String string) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String s : from.parse(string)) {
+                s = s.toLowerCase();
+                stringBuilder.append(s).append("_");
+            }
+            return stringBuilder.substring(0, stringBuilder.length() -1);
         }
 
         @Override
         public String[] parse(String string) {
-            if (string == null || string.isEmpty()) {
-                return new String[0];
-            } else if (!this.is(string)) {
-                return new String[0];
-            } else {
-                return string.split("_");
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be snake_case.");
             }
+            return string.split("_");
         }
 
         @Override
         public boolean is(String string) {
-            return false;
+            return string != null && !string.isEmpty() && string.matches("^([A-Za-z]+_)+[A-Za-z]+$");
         }
+
+        @Override
+        public String append(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be snake_case.");
+            }
+            return string + "_" + snippet.toLowerCase();
+        }
+
+        @Override
+        public String prepend(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be snake_case.");
+            }
+            return snippet.toLowerCase() + "_" + string;
+        }
+
     },
     KEBAB {
         @Override
-        public String convert(StringCaseUtil from, String string) {
-            return null;
+        public String transform(StringCaseUtil from, String string) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String s : from.parse(string)) {
+                s = s.toLowerCase();
+                stringBuilder.append(s).append("-");
+            }
+            return stringBuilder.substring(0, stringBuilder.length() -1);
         }
 
         @Override
         public String[] parse(String string) {
-            if (string == null || string.isEmpty()) {
-                return new String[0];
-            } else if (!this.is(string)) {
-                return new String[0];
-            } else {
-                return string.split("-");
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be kebab-case.");
             }
+            return string.split("-");
         }
 
         @Override
         public boolean is(String string) {
-            return false;
+            return string != null && !string.isEmpty() && string.matches("^([A-Za-z]+-)+[A-Za-z]+$");
+        }
+
+        @Override
+        public String append(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be kebab-case.");
+            }
+            return string + "-" + snippet.toLowerCase();
+        }
+
+        @Override
+        public String prepend(String string, String snippet) {
+            if (!this.is(string)) {
+                throw new StringFormatException("String does not appear to be kebab-case.");
+            }
+            return snippet.toLowerCase() + "-" + string;
         }
     };
 
-    public abstract String convert(StringCaseUtil from, String string);
+    public abstract String transform(StringCaseUtil from, String string);
     public abstract String[] parse(String string);
     public abstract boolean is(String string);
+    public abstract String append(String string, String snippet);
+    public abstract String prepend(String string, String snippet);
+
+    public static class StringFormatException extends RuntimeException {
+        public StringFormatException(String message) {
+            super(message);
+        }
+    }
+
+    public static StringCaseUtil which(String string) {
+        for (StringCaseUtil stringCase : values()) {
+            if (stringCase.is(string)) {
+                return stringCase;
+            }
+        }
+        throw new StringFormatException("String format not understood.");
+    }
+
+    public static String convert(StringCaseUtil to, String string) {
+        return to.transform(StringCaseUtil.which(string), string);
+    }
 }
--- src/test/java/name/ben/shepley/fsql/util/StringCaseUtilTest.java
+++ src/test/java/name/ben/shepley/fsql/util/StringCaseUtilTest.java
@@ -6,65 +6,104 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class StringCaseUtilTest {
-    private static final int TEST_NO_RESULT = 0;
-    private static final String TEST_NULL_STRING = null;
-    private static final String TEST_EMPTY_STRING = "";
-
-    private static final int TEST_SINGLE_WORD_LENGTH = 1;
-    private static final String TEST_SINGLE_WORD_LOWER_CASE = "the";
-    private static final String TEST_SINGLE_WORD_UPPER_CASE = "THE";
-
     private static final int TEST_MULTI_WORD_STRING_LENGTH = 9;
-    private static final String TEST_CAMEL_STRING =  "theQuickBrownFoxJumpsOverTheLazyDog";
-    private static final String TEST_PASCAL_STRING = "TheQuickBrownFoxJumpsOverTheLazyDog";
-    private static final String TEST_SNAKE_STRING =  "the_quick_brown_fox_jumps_over_the_lazy_dog";
-    private static final String TEST_KEBAB_STRING =  "the-quick-brown-fox-jumps-over-the-lazy-dog";
+
+    private static final String TEST_CAMEL_STRING           = "theQuickBrownFoxJumpsOverTheLazyDog";
+    private static final String TEST_PREPEND_CAMEL_STRING   = "andTheQuickBrownFoxJumpsOverTheLazyDog";
+    private static final String TEST_APPEND_CAMEL_STRING    = "theQuickBrownFoxJumpsOverTheLazyDogEnd";
+
+    private static final String TEST_PASCAL_STRING          = "TheQuickBrownFoxJumpsOverTheLazyDog";
+    private static final String TEST_PREPEND_PASCAL_STRING  = "AndTheQuickBrownFoxJumpsOverTheLazyDog";
+    private static final String TEST_APPEND_PASCAL_STRING   = "TheQuickBrownFoxJumpsOverTheLazyDogEnd";
+
+    private static final String TEST_SNAKE_STRING           =  "the_quick_brown_fox_jumps_over_the_lazy_dog";
+    private static final String TEST_PREPEND_SNAKE_STRING   =  "and_the_quick_brown_fox_jumps_over_the_lazy_dog";
+    private static final String TEST_APPEND_SNAKE_STRING    =  "the_quick_brown_fox_jumps_over_the_lazy_dog_end";
+
+    private static final String TEST_KEBAB_STRING           =  "the-quick-brown-fox-jumps-over-the-lazy-dog";
+    private static final String TEST_PREPEND_KEBAB_STRING   =  "and-the-quick-brown-fox-jumps-over-the-lazy-dog";
+    private static final String TEST_APPEND_KEBAB_STRING    =  "the-quick-brown-fox-jumps-over-the-lazy-dog-end";
 
     @Test
     public void testCamel() {
-        /* Test Convert */
-        assertThat(StringCaseUtil.CAMEL.convert(StringCaseUtil.CAMEL, TEST_NULL_STRING), blankString());
-        assertThat(StringCaseUtil.CAMEL.convert(StringCaseUtil.CAMEL, TEST_EMPTY_STRING), blankString());
-        assertThat(StringCaseUtil.CAMEL.convert(StringCaseUtil.CAMEL, TEST_SINGLE_WORD_LOWER_CASE), equalTo(TEST_SINGLE_WORD_LOWER_CASE));
-        assertThat(StringCaseUtil.CAMEL.convert(StringCaseUtil.CAMEL, TEST_SINGLE_WORD_UPPER_CASE), blankString());
-        assertThat(StringCaseUtil.CAMEL.convert(StringCaseUtil.CAMEL, TEST_CAMEL_STRING), equalTo(TEST_CAMEL_STRING));
-        //assertThat(StringFormatter.CAMEL.convert(StringFormatter.CAMEL, TEST_PASCAL_STRING), equalTo(TEST_CAMEL_STRING));
-        //assertThat(StringFormatter.CAMEL.convert(StringFormatter.CAMEL, TEST_SNAKE_STRING), equalTo(TEST_CAMEL_STRING));
-        //assertThat(StringFormatter.CAMEL.convert(StringFormatter.CAMEL, TEST_KEBAB_STRING), equalTo(TEST_CAMEL_STRING));
+        assertThat(StringCaseUtil.CAMEL.transform(StringCaseUtil.CAMEL, TEST_CAMEL_STRING), equalTo(TEST_CAMEL_STRING));
+        assertThat(StringCaseUtil.CAMEL.transform(StringCaseUtil.PASCAL, TEST_PASCAL_STRING), equalTo(TEST_CAMEL_STRING));
+        assertThat(StringCaseUtil.CAMEL.transform(StringCaseUtil.SNAKE, TEST_SNAKE_STRING), equalTo(TEST_CAMEL_STRING));
+        assertThat(StringCaseUtil.CAMEL.transform(StringCaseUtil.KEBAB, TEST_KEBAB_STRING), equalTo(TEST_CAMEL_STRING));
 
-        /* Test Parse */
-        assertThat(StringCaseUtil.CAMEL.parse(TEST_NULL_STRING), arrayWithSize(TEST_NO_RESULT));
-        assertThat(StringCaseUtil.CAMEL.parse(TEST_EMPTY_STRING), arrayWithSize(TEST_NO_RESULT));
-        assertThat(StringCaseUtil.CAMEL.parse(TEST_SINGLE_WORD_LOWER_CASE), arrayWithSize(TEST_SINGLE_WORD_LENGTH));
-        assertThat(StringCaseUtil.CAMEL.parse(TEST_SINGLE_WORD_UPPER_CASE), arrayWithSize(TEST_NO_RESULT));
+        assertThat(StringCaseUtil.CAMEL.prepend(TEST_CAMEL_STRING, "and"), equalTo(TEST_PREPEND_CAMEL_STRING));
+        assertThat(StringCaseUtil.CAMEL.append(TEST_CAMEL_STRING, "end"), equalTo(TEST_APPEND_CAMEL_STRING));
+
         assertThat(StringCaseUtil.CAMEL.parse(TEST_CAMEL_STRING), arrayWithSize(TEST_MULTI_WORD_STRING_LENGTH));
-        assertThat(StringCaseUtil.CAMEL.parse(TEST_PASCAL_STRING), arrayWithSize(TEST_NO_RESULT));
-        assertThat(StringCaseUtil.CAMEL.parse(TEST_SNAKE_STRING), arrayWithSize(TEST_NO_RESULT));
-        assertThat(StringCaseUtil.CAMEL.parse(TEST_KEBAB_STRING), arrayWithSize(TEST_NO_RESULT));
-
-        /* Test Is */
-        assertThat(StringCaseUtil.CAMEL.is(TEST_NULL_STRING), is(false));
-        assertThat(StringCaseUtil.CAMEL.is(TEST_EMPTY_STRING), is(false));
-        assertThat(StringCaseUtil.CAMEL.is(TEST_SINGLE_WORD_LOWER_CASE), is(true));
-        assertThat(StringCaseUtil.CAMEL.is(TEST_SINGLE_WORD_UPPER_CASE), is(false));
         assertThat(StringCaseUtil.CAMEL.is(TEST_CAMEL_STRING), is(true));
-        assertThat(StringCaseUtil.CAMEL.is(TEST_PASCAL_STRING), is(false));
-        assertThat(StringCaseUtil.CAMEL.is(TEST_SNAKE_STRING), is(false));
-        assertThat(StringCaseUtil.CAMEL.is(TEST_KEBAB_STRING), is(false));
     }
 
     @Test
     public void testPascal() {
+        assertThat(StringCaseUtil.PASCAL.transform(StringCaseUtil.CAMEL, TEST_CAMEL_STRING), equalTo(TEST_PASCAL_STRING));
+        assertThat(StringCaseUtil.PASCAL.transform(StringCaseUtil.PASCAL, TEST_PASCAL_STRING), equalTo(TEST_PASCAL_STRING));
+        assertThat(StringCaseUtil.PASCAL.transform(StringCaseUtil.SNAKE, TEST_SNAKE_STRING), equalTo(TEST_PASCAL_STRING));
+        assertThat(StringCaseUtil.PASCAL.transform(StringCaseUtil.KEBAB, TEST_KEBAB_STRING), equalTo(TEST_PASCAL_STRING));
 
+        assertThat(StringCaseUtil.PASCAL.prepend(TEST_PASCAL_STRING, "and"), equalTo(TEST_PREPEND_PASCAL_STRING));
+        assertThat(StringCaseUtil.PASCAL.append(TEST_PASCAL_STRING, "end"), equalTo(TEST_APPEND_PASCAL_STRING));
+
+        assertThat(StringCaseUtil.PASCAL.parse(TEST_PASCAL_STRING), arrayWithSize(TEST_MULTI_WORD_STRING_LENGTH));
+        assertThat(StringCaseUtil.PASCAL.is(TEST_PASCAL_STRING), is(true));
     }
 
     @Test
     public void testSnake() {
+        assertThat(StringCaseUtil.SNAKE.transform(StringCaseUtil.CAMEL, TEST_CAMEL_STRING), equalTo(TEST_SNAKE_STRING));
+        assertThat(StringCaseUtil.SNAKE.transform(StringCaseUtil.PASCAL, TEST_PASCAL_STRING), equalTo(TEST_SNAKE_STRING));
+        assertThat(StringCaseUtil.SNAKE.transform(StringCaseUtil.SNAKE, TEST_SNAKE_STRING), equalTo(TEST_SNAKE_STRING));
+        assertThat(StringCaseUtil.SNAKE.transform(StringCaseUtil.KEBAB, TEST_KEBAB_STRING), equalTo(TEST_SNAKE_STRING));
 
+        assertThat(StringCaseUtil.SNAKE.prepend(TEST_SNAKE_STRING, "and"), equalTo(TEST_PREPEND_SNAKE_STRING));
+        assertThat(StringCaseUtil.SNAKE.append(TEST_SNAKE_STRING, "end"), equalTo(TEST_APPEND_SNAKE_STRING));
+
+        assertThat(StringCaseUtil.SNAKE.parse(TEST_SNAKE_STRING), arrayWithSize(TEST_MULTI_WORD_STRING_LENGTH));
+        assertThat(StringCaseUtil.SNAKE.is(TEST_SNAKE_STRING), is(true));
     }
 
     @Test
     public void testKebab() {
+        assertThat(StringCaseUtil.KEBAB.transform(StringCaseUtil.CAMEL, TEST_CAMEL_STRING), equalTo(TEST_KEBAB_STRING));
+        assertThat(StringCaseUtil.KEBAB.transform(StringCaseUtil.PASCAL, TEST_PASCAL_STRING), equalTo(TEST_KEBAB_STRING));
+        assertThat(StringCaseUtil.KEBAB.transform(StringCaseUtil.SNAKE, TEST_SNAKE_STRING), equalTo(TEST_KEBAB_STRING));
+        assertThat(StringCaseUtil.KEBAB.transform(StringCaseUtil.KEBAB, TEST_KEBAB_STRING), equalTo(TEST_KEBAB_STRING));
 
+        assertThat(StringCaseUtil.KEBAB.prepend(TEST_KEBAB_STRING, "and"), equalTo(TEST_PREPEND_KEBAB_STRING));
+        assertThat(StringCaseUtil.KEBAB.append(TEST_KEBAB_STRING, "end"), equalTo(TEST_APPEND_KEBAB_STRING));
+
+        assertThat(StringCaseUtil.KEBAB.parse(TEST_KEBAB_STRING), arrayWithSize(TEST_MULTI_WORD_STRING_LENGTH));
+        assertThat(StringCaseUtil.KEBAB.is(TEST_KEBAB_STRING), is(true));
+    }
+
+    @Test
+    public void testWhich() {
+        assertThat(StringCaseUtil.which(TEST_CAMEL_STRING), equalTo(StringCaseUtil.CAMEL));
+        assertThat(StringCaseUtil.which(TEST_PASCAL_STRING), equalTo(StringCaseUtil.PASCAL));
+        assertThat(StringCaseUtil.which(TEST_SNAKE_STRING), equalTo(StringCaseUtil.SNAKE));
+        assertThat(StringCaseUtil.which(TEST_KEBAB_STRING), equalTo(StringCaseUtil.KEBAB));
+    }
+
+    @Test
+    public void testConvert() {
+        assertThat(StringCaseUtil.convert(StringCaseUtil.CAMEL, TEST_PASCAL_STRING), equalTo(TEST_CAMEL_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.CAMEL, TEST_SNAKE_STRING), equalTo(TEST_CAMEL_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.CAMEL, TEST_KEBAB_STRING), equalTo(TEST_CAMEL_STRING));
+
+        assertThat(StringCaseUtil.convert(StringCaseUtil.PASCAL, TEST_CAMEL_STRING), equalTo(TEST_PASCAL_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.PASCAL, TEST_SNAKE_STRING), equalTo(TEST_PASCAL_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.PASCAL, TEST_KEBAB_STRING), equalTo(TEST_PASCAL_STRING));
+
+        assertThat(StringCaseUtil.convert(StringCaseUtil.SNAKE, TEST_CAMEL_STRING), equalTo(TEST_SNAKE_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.SNAKE, TEST_PASCAL_STRING), equalTo(TEST_SNAKE_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.SNAKE, TEST_KEBAB_STRING), equalTo(TEST_SNAKE_STRING));
+
+        assertThat(StringCaseUtil.convert(StringCaseUtil.KEBAB, TEST_CAMEL_STRING), equalTo(TEST_KEBAB_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.KEBAB, TEST_PASCAL_STRING), equalTo(TEST_KEBAB_STRING));
+        assertThat(StringCaseUtil.convert(StringCaseUtil.KEBAB, TEST_SNAKE_STRING), equalTo(TEST_KEBAB_STRING));
     }
 }
